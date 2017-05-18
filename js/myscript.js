dd3fb4665bde73d97f9fbb40ea3188498b00a16e
xenderx <vikyaiyer1997@gmail.com>
2017-05-18 12:00:22 +0530

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function chk_pass()
+{
+    if (document.getElementById('password').value ==
+            document.getElementById('confirm_password').value) {
+        document.getElementById('submit').disabled = false;
+    } else {
+        document.getElementById('submit').disabled = true;
+    }
+}
